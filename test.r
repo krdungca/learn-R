239bcd8d49218d2a5c589f0cefae4e5df9308fc4
krdungca14 <ken.rodulfo.dungca@gmail.com>
2024-09-28 16:30:28 +0800

    import, update

--- test.r
+++ test.r
@@ -8,9 +8,27 @@ sapply(mtcars,sum)
 
 mtcars
 
+library(dbplyr)
+library(nycflights13)
 #data manipulation in R
 #dplyr/ Deep Flyer
 
 #compbio is 80% data cleaning, 20% analysis
 
+#str( ) --> display all the columns presentstr
+
 #common operations
+
+#flights
+
+#library (dbplyr)
+
+#select, filter, mutate
+
+#mutate(flights,
+#       gain = arr_delay - dep_delay,
+#       speed = distance / air_time * 60       
+#       )
+# good technique.. create a new variable  then assign <- the mutated function to that variable
+
+#arrange()
