f19aa3fbd55635efa1ebf5d74377ea019a97931b
krdungca14 <ken.rodulfo.dungca@gmail.com>
2024-09-28 15:40:37 +0800

    Update test.r

--- test.r
+++ test.r
@@ -2,3 +2,15 @@ head(mtcars)
 tail(mtcars)
 summary(mtcars)
 str(mtcars)
+
+sapply(mtcars,sum)
+
+
+mtcars
+
+#data manipulation in R
+#dplyr/ Deep Flyer
+
+#compbio is 80% data cleaning, 20% analysis
+
+#common operations
